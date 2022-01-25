e81e99a97802ba4d69b4cb4eb322eaa8867b4a84
Reguel Bock <reguel87@gmail.com>
2022-01-25 16:02:56 +0100

    [DEV-248] file upload size and type validation

--- apps/backend/src/file/file.service.ts
+++ apps/backend/src/file/file.service.ts
@@ -1,17 +1,25 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotAcceptableException } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import FileType from 'file-type';
 import { ReadStream } from 'fs';
 import { Stream } from 'stream';
 import { S3Service } from './s3.service';
 
+const pictureFilenameStart = "cover";
+const allowedPicturesRegEx = new RegExp("([a-zA-Z0-9\\s_\\.\\-\\(\\):])+(.jpg|.jpeg|.png|.gif)$");
+const allowedMusicNftsRegEx = new RegExp("([a-zA-Z0-9\\s_\\.\\-\\(\\):])+(.mp3|.wave|.wav|.flac)$");
+const maxAllowedFileSize = 1000000;
+
 @Injectable()
 export class FileService {
-  constructor(private configService: ConfigService, private s3Service: S3Service) {}
+  constructor(private configService: ConfigService, private s3Service: S3Service) { }
 
-  async uploadFileToBucket(fileName, bucket, createReadStream): Promise<string> {
+  async uploadFileToBucket(fileName: string, bucket: string, createReadStream): Promise<string> {
     const writeStream = new Stream.PassThrough();
     const fileTypeStream = await FileType.stream(createReadStream());
+
+    this.validateFileSize(fileName, fileTypeStream);
+    this.validateFileToUpload(fileName, fileTypeStream);
 
     const uploadFile = this.s3Service.uploadFile(writeStream, fileName, bucket, {
       ACL: 'public-read',
@@ -29,7 +37,26 @@ export class FileService {
     return await uploadFile;
   }
 
-  getAWSReadStream(bucket, fileName): ReadStream {
+  private async validateFileSize(fileName: string, fileTypeStream) {
+    let byteLength = 0;
+    for await (const uploadChunk of fileTypeStream) {
+      byteLength += (uploadChunk as Buffer).byteLength;
+      if (byteLength > maxAllowedFileSize) {
+        throw new NotAcceptableException("File with name " + fileName + " is too large to upload. Maximum allowed filesize is " + maxAllowedFileSize / 10000 + "MB.");
+      }
+    }
+    fileTypeStream.destroy();
+  }
+
+  private validateFileToUpload(fileName: string, fileTypeStream) {
+    if (fileName.startsWith(pictureFilenameStart) && !fileTypeStream.fileType.mime.match(allowedPicturesRegEx)) {
+      throw new NotAcceptableException('Picture file of type ' + fileTypeStream.fileType.mime + ' is not acceptable for this parameter.');
+    } else if (!fileName.startsWith(pictureFilenameStart) && !fileTypeStream.fileType.mime.match(allowedMusicNftsRegEx)) {
+      throw new NotAcceptableException('Music NFT file of type ' + fileTypeStream.fileType.mime + ' is not acceptable for this parameter.');
+    }
+  }
+
+  getAWSReadStream(bucket: string, fileName: string): ReadStream {
     const awsReadStream: ReadStream = this.s3Service.getFileReadStream(bucket, fileName);
     return awsReadStream;
   }
