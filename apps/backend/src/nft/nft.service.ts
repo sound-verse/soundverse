021046abf6247c6068aa15b1eb31b0df105c829b
Reguel Bock <reguel87@gmail.com>
2022-02-17 13:18:39 +0100

    [DEV-293] refactoring

--- apps/backend/src/nft/nft.service.ts
+++ apps/backend/src/nft/nft.service.ts
@@ -137,18 +137,14 @@ export class NftService {
   }
 
   async getNfts(limitOfDocuments: number, documentsToSkip: number = 0, filter?: NftsFilter): Promise<Nft[]> {
-    console.log("limitOfDocuments: " + limitOfDocuments)
-    console.log("documentsToSkip: " + documentsToSkip)
 
     if (filter?.creatorEthAddress) {
-      console.log("in if")
       const creator = await this.userService.findByETHAddress(filter.creatorEthAddress.toLowerCase());
       if (!creator) {
         return null;
       }
       return this.nftModel.find({ verified: true, creator: creator._id, tokenId: { $exists: true } });
     }
-
 
     var findQuery = this.nftModel
     .find({ verified: true})
@@ -161,7 +157,6 @@ export class NftService {
 
     const results = await findQuery;
 
-    console.log("Results" + results.length);
     return results
   }
 }
