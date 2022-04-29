6cbcfee593dc053df89187b975130d9c672c8164
Michael Lodzik <michael.lodzik@gmail.com>
2022-04-29 16:47:35 +0200

    fix: types

--- apps/frontend/pages/master/[id].tsx
+++ apps/frontend/pages/master/[id].tsx
@@ -4,8 +4,7 @@ import { User } from '../../hooks/useProfile'
 import Custom404 from '../404'
 import { GET_NFT } from '../../common/graphql/queries/get-nft.query'
 import SingleNftPage from '../../components/SingleNftPage/SingleNftPage'
-import { Nft, Selling } from '../../common/graphql/schema.d'
-import { NftType } from '../../common/types/nft-type.enum'
+import { Nft, Selling, NftType } from '../../common/graphql/schema.d'
 
 type ProfileProps = {
   query: {
