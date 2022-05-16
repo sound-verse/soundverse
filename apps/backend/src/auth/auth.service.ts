52ae2d7667de8b0f99b5d8a64d1f551aabd13d69
MichaelLod <michael.lodzik@gmail.com>
2022-05-16 15:53:28 +0200

    feat: lift testnet restrictions

--- apps/backend/src/auth/auth.service.ts
+++ apps/backend/src/auth/auth.service.ts
@@ -30,32 +30,32 @@ export class AuthService {
       throw new ForbiddenException('Signature error.');
     }
 
-    const authorizedWallets = [
-      '0xE39569EF2A516f0CA065a8dA698C79EE739D02c1',
-      '0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266',
-      '0x52122acDfED5740019765889766BF9Bfd71a66f1',
-      '0x955F5C98058ab50623a7a4aa1dD9C55372C79a5a',
-      '0x50Ef709c97468ae31E5F6b7089A078C9a05197FD',
-      '0xDa3b8108161CdB458a9d233498E1Aaf315EfC77e',
-      '0xB46f1b478983B127FEEA9643DDc8560AF2Cd3724',
-      '0x73A54343f2b73A6DC749f138690EB115A3501c5D',
-      '0x457b00914C44F08fEA1A362521b9240FE3945449',
-      '0x46fB855DA9D974d3720215eFF2687e065aE39612',
-      '0x56f317fE462555cE9d9306a10437386A2B33D5B7',
-      '0x140df23439bA3b1Aab31bA0a5295B86034cD69Db',
-      '0x2546BcD3c84621e976D8185a91A922aE77ECEc30',
-      '0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266',
-      '0x70997970C51812dc3A010C7d01b50e0d17dc79C8',
-      '0x71bE63f3384f5fb98995898A86B02Fb2426c5788',
-      '0xfA5035568d44ae5c77A456C9a4142805DdeA3531',
-    ];
+    // const authorizedWallets = [
+    //   '0xE39569EF2A516f0CA065a8dA698C79EE739D02c1',
+    //   '0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266',
+    //   '0x52122acDfED5740019765889766BF9Bfd71a66f1',
+    //   '0x955F5C98058ab50623a7a4aa1dD9C55372C79a5a',
+    //   '0x50Ef709c97468ae31E5F6b7089A078C9a05197FD',
+    //   '0xDa3b8108161CdB458a9d233498E1Aaf315EfC77e',
+    //   '0xB46f1b478983B127FEEA9643DDc8560AF2Cd3724',
+    //   '0x73A54343f2b73A6DC749f138690EB115A3501c5D',
+    //   '0x457b00914C44F08fEA1A362521b9240FE3945449',
+    //   '0x46fB855DA9D974d3720215eFF2687e065aE39612',
+    //   '0x56f317fE462555cE9d9306a10437386A2B33D5B7',
+    //   '0x140df23439bA3b1Aab31bA0a5295B86034cD69Db',
+    //   '0x2546BcD3c84621e976D8185a91A922aE77ECEc30',
+    //   '0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266',
+    //   '0x70997970C51812dc3A010C7d01b50e0d17dc79C8',
+    //   '0x71bE63f3384f5fb98995898A86B02Fb2426c5788',
+    //   '0xfA5035568d44ae5c77A456C9a4142805DdeA3531',
+    // ];
 
-    if (
-      this.configService.get('ENVIRONMENT') !== 'local' &&
-      !authorizedWallets.find((authAddress) => address.toLowerCase() === authAddress.toLowerCase())
-    ) {
-      throw new ForbiddenException('Wallet not authorized.');
-    }
+    // if (
+    //   this.configService.get('ENVIRONMENT') !== 'local' &&
+    //   !authorizedWallets.find((authAddress) => address.toLowerCase() === authAddress.toLowerCase())
+    // ) {
+    //   throw new ForbiddenException('Wallet not authorized.');
+    // }
     user.nonce = Math.floor(Math.random() * 1000000);
     await user.save();
 
--- apps/frontend/components/layout.tsx
+++ apps/frontend/components/layout.tsx
@@ -10,13 +10,13 @@ export default function Layout({ children }) {
   return (
     <div>
       <Header />
-      {process.env.NEXT_PUBLIC_ENVIRONMENT === 'local' || authUser ? (
+      {/* {process.env.NEXT_PUBLIC_ENVIRONMENT === 'local' || authUser ? (
         <div className="w-full p-10 mx-auto">{children}</div>
-      ) : (
-        <div className="text-white text-2xl font-bold flex h-screen justify-center self-center items-center -mt-36">
-          Only authorized wallets are able to use this page.
-        </div>
-      )}
+      ) : ( */}
+      <div className="text-white text-2xl font-bold flex h-screen justify-center self-center items-center -mt-36">
+        Only authorized wallets are able to use this page.
+      </div>
+      {/* )} */}
     </div>
   )
 }
