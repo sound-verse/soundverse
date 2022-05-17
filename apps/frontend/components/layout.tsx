1fd62c868b772985a2439e8a4f0da3d12524d724
MichaelLod <michael.lodzik@gmail.com>
2022-05-17 09:57:58 +0200

    fix: desktop only

--- apps/frontend/components/layout.tsx
+++ apps/frontend/components/layout.tsx
@@ -2,15 +2,23 @@ import Header from '../components/Header'
 import Footer from '../components/Footer'
 import { useAuthContext } from '../context/AuthContext'
 import { useAudioContext } from '../context/AudioContext'
+import useWindowDimensions from '../hooks/useWindowDimensions'
 
 export default function Layout({ children }) {
-  const { authUser } = useAuthContext()
-  const { currentTrack } = useAudioContext()
+  const { width } = useWindowDimensions()
 
   return (
     <div>
-      <Header />
-      <div className="w-full p-10 mx-auto">{children}</div>
+      {width < 1160 ? (
+        <div className="text-white flex items-center justify-center h-screen">
+          This page supports desktop only currently.
+        </div>
+      ) : (
+        <>
+          <Header />
+          <div className="w-full p-10 mx-auto">{children}</div>
+        </>
+      )}
     </div>
   )
 }
