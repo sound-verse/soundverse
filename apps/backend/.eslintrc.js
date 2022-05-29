1bae03788180f7ed6c6b04b0a3196e87200f0549
MichaelLod <michael.lodzik@gmail.com>
2022-05-29 18:09:27 +0200

    fix: eslint

--- apps/backend/.eslintrc.js
+++ apps/backend/.eslintrc.js
@@ -4,4 +4,9 @@ module.exports = {
     project: ['./tsconfig.json'],
     tsconfigRootDir: __dirname,
   },
+  rules: {
+    '@typescript-eslint/no-unused-vars': 'off',
+    '@typescript-eslint/camelcase': 'off',
+    '@typescript-eslint/no-use-before-define': 'off',
+  },
 };
