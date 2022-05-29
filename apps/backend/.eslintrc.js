2095aa37ad0968ea3fd34af94d42e308cba5ca0b
MichaelLod <michael.lodzik@gmail.com>
2022-05-29 18:12:21 +0200

    fix: eslint

--- apps/backend/.eslintrc.js
+++ apps/backend/.eslintrc.js
@@ -8,5 +8,6 @@ module.exports = {
     '@typescript-eslint/no-unused-vars': 'off',
     '@typescript-eslint/camelcase': 'off',
     '@typescript-eslint/no-use-before-define': 'off',
+    '@typescript-eslint/interface-name-prefix': 'off',
   },
 };
