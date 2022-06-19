5917afaef2db996d69709c6d599f0b6d49473b82
MichaelLod <michael.lodzik@gmail.com>
2022-06-19 17:42:29 +0200

    fix: better minting page

--- apps/frontend/components/create/CreateForm.tsx
+++ apps/frontend/components/create/CreateForm.tsx
@@ -8,6 +8,7 @@ import toast from 'react-hot-toast'
 import Modal from 'react-modal'
 import { Bars } from 'react-loader-spinner'
 import styles from './CreateForm.module.css'
+import cn from 'classnames'
 
 const FILE_SIZE = 100000000
 
@@ -17,6 +18,18 @@ export const SUPPORTED_FORMATS_PICTURE = [
   'image/gif',
   'image/png',
 ]
+
+interface FirstStepValues {
+  name: string
+  description: string
+  tags: string[]
+}
+
+interface SecondStepValues {
+  licenses: number
+  royaltyFeeMaster: number
+  creatorOwnerSplit: number
+}
 
 export const SUPPORTED_FORMATS_MUSIC = ['audio/mpeg', 'audio/wav']
 
@@ -28,15 +41,19 @@ export const CreateForm = () => {
   const { prepareMint } = useCreateNFT()
   const [nftFileError, setNftFileError] = useState<String>('')
   const [pictureFileError, setPictureFileError] = useState<String>('')
+  const [firstStepValues, setFirstStepValues] = useState<FirstStepValues>()
+  const [showSecondStep, setShowSecondStep] = useState(false)
 
-  const initialValues = {
+  const initialValuesFirstStep: FirstStepValues = {
     name: '',
     description: '',
     tags: [],
-    licenses: 2,
-    royaltyFeeMaster: 0,
-    royaltyFeeLicense: 0,
-    creatorOwnerSplit: 0,
+  }
+
+  const initialValuesSecondStep: SecondStepValues = {
+    licenses: undefined,
+    royaltyFeeMaster: undefined,
+    creatorOwnerSplit: undefined,
   }
 
   const onFileChange = (
@@ -63,11 +80,14 @@ export const CreateForm = () => {
     setFileError('')
   }
 
-  const validationSchema = Yup.object().shape({
+  const validationSchemaFirstStep = Yup.object().shape({
     name: Yup.string().required('Please enter a title'),
     description: Yup.string()
       .required('Please enter a description')
       .max(1000, 'Maximum number of characters of 1000 exceeded'),
+  })
+
+  const validationSchemaSecondStep = Yup.object().shape({
     licenses: Yup.number()
       .typeError('Please enter a number')
       .min(2, 'You have to set a minium of 2 licenses')
@@ -75,11 +95,7 @@ export const CreateForm = () => {
       .required('Please enter a number'),
     royaltyFeeMaster: Yup.number()
       .typeError('Please enter a number')
-      .max(100, 'You can enter a number up to 100')
-      .required('Please enter a number'),
-    royaltyFeeLicense: Yup.number()
-      .typeError('Please enter a number')
-      .max(100, 'You can enter a number up to 100')
+      .max(50, 'You can enter a number up to 50')
       .required('Please enter a number'),
     creatorOwnerSplit: Yup.number()
       .typeError('Please enter a number')
@@ -87,7 +103,7 @@ export const CreateForm = () => {
       .required('Please enter a number'),
   })
 
-  const onSubmit = async (values, onSubmitProps) => {
+  const submitFirstStep = (values) => {
     if (!nftFile) {
       setNftFileError('Please select a music file')
       return
@@ -97,16 +113,22 @@ export const CreateForm = () => {
       setPictureFileError('Please select a picture file')
       return
     }
+
+    setFirstStepValues(values)
+    setShowSecondStep(true)
+  }
+
+  const onSubmit = async (values, onSubmitProps) => {
     try {
       setShowing(true)
       const { id } = await prepareMint({
         nftFile,
         pictureFile,
-        name: values.name,
-        description: values.description,
+        name: firstStepValues.name,
+        description: firstStepValues.description,
         licenses: values.licenses,
         royaltyFeeMaster: values.royaltyFeeMaster,
-        royaltyFeeLicense: values.royaltyFeeLicense,
+        royaltyFeeLicense: values.royaltyFeeMaster,
         creatorOwnerSplit: values.creatorOwnerSplit,
       })
       if (id) {
@@ -126,183 +148,244 @@ export const CreateForm = () => {
 
   Modal.setAppElement('#__next')
 
+  const renderFirstStep = () => (
+    <Formik
+      initialValues={initialValuesFirstStep}
+      validationSchema={validationSchemaFirstStep}
+      onSubmit={submitFirstStep}
+      enableReinitialize
+    >
+      <Form>
+        <div className="flex flex-col">
+          <div className="text-white font-bold text-sm">Track</div>
+          <div>
+            <label htmlFor="nftFile" className={styles.buttonStyle}>
+              Choose Music File
+            </label>
+            <Field
+              type="file"
+              id="nftFile"
+              name="nftFile"
+              className="hidden"
+              onChange={(e) =>
+                onFileChange(
+                  e,
+                  setNftFile,
+                  setNftFileError,
+                  SUPPORTED_FORMATS_MUSIC
+                )
+              }
+            ></Field>
+            <div className="text-grey-light mt-3 text-xs">
+              MP3, WAVE - Max 100Mb
+            </div>
+            <div className="text-grey-light text-xs">
+              {nftFile && `Selected File: ${nftFile.name}`}
+            </div>
+            <div className={styles.error}>{nftFileError}</div>
+          </div>
+          <div>
+            <label htmlFor="pictureFile" className={styles.buttonStyle}>
+              Choose Cover Picture
+            </label>
+            <input
+              type="file"
+              id="pictureFile"
+              name="pictureFile"
+              className="hidden"
+              onChange={(e) =>
+                onFileChange(
+                  e,
+                  setPictureFile,
+                  setPictureFileError,
+                  SUPPORTED_FORMATS_PICTURE
+                )
+              }
+            ></input>
+            <div className="text-grey-light mt-3 text-xs">
+              JPG, PNG - Max 100Mb
+            </div>
+          </div>
+          <div className="text-grey-light text-xs">
+            {pictureFile && `Selected File: ${pictureFile.name}`}
+          </div>
+          <div className={styles.error}>{pictureFileError}</div>
+          <div className="text-white font-bold text-sm mt-10">Track Name</div>
+          <div className="mt-3 w-full">
+            <Field
+              id="name"
+              name="name"
+              placeholder="Ice in the dark..."
+              className="outline-none bg-grey-dark text-white w-full text-sm"
+            />
+            <div className="border-t-2 w-full mt-2 border-grey-medium opacity-50"></div>
+            <div className="text-grey-light mt-2 text-xs">
+              max. 20 characters
+            </div>
+            <div className={styles.error}>
+              <ErrorMessage name="name" />
+            </div>
+          </div>
+          <div className="text-white font-bold text-sm mt-10">Description</div>
+          <div className="mt-3 text-sm">
+            <Field
+              type="input"
+              as="textarea"
+              name="description"
+              className={styles.descriptionField}
+              id="trac-desc"
+              placeholder="I am ..."
+              rows={8}
+              cols={50}
+            ></Field>
+            <div className={styles.error}>
+              <ErrorMessage name="description" />
+            </div>
+          </div>
+          <button className={styles.mintButton} type="submit">
+            Next
+          </button>
+        </div>
+      </Form>
+    </Formik>
+  )
+
+  const renderSecondStep = () => (
+    <Formik
+      initialValues={initialValuesSecondStep}
+      validationSchema={validationSchemaSecondStep}
+      onSubmit={onSubmit}
+      enableReinitialize
+    >
+      <Form>
+        <div
+          className="text-white mb-10 cursor-pointer"
+          onClick={() => setShowSecondStep(false)}
+        >
+          {'<-'} Back
+        </div>
+        <div className="flex flex-col text-sm text-white">
+          <div className="font-bold mb-2">
+            Royalty Settings for secondary sales of Master and Licenses
+          </div>
+          <div className="text-grey-light leading-6">
+            Set your royalty percentage you would like to receive on every
+            secondary trade of your Master and Licences.{' '}
+            <span className="text-purple underline">
+              Caution: You won’t be able to change this royalty percentage after
+              minting you NFT!
+            </span>
+          </div>
+          <div className="text-white font-bold text-sm mt-10">
+            Secondary royalty
+          </div>
+          <div className="mt-6 w-full">
+            <Field
+              id="royaltyFeeMaster"
+              name="royaltyFeeMaster"
+              placeholder="5%, 10%, 15%, ..."
+              className="outline-none bg-grey-dark text-white w-full"
+            />
+            <div className="border-t-2 w-full mt-2 border-grey-medium opacity-50"></div>
+            <div className="text-grey-light mt-2 text-xs">
+              5-15% are industry standards - Max is 50%
+              <div className={styles.error}>
+                <ErrorMessage name="royaltyFeeMaster" />
+              </div>
+            </div>
+          </div>
+          <div className="text-white font-bold text-sm mt-10">Licenses</div>
+          <div className="mt-3 w-full">
+            <Field
+              id="licenses"
+              name="licenses"
+              placeholder="10000"
+              className="outline-none bg-grey-dark text-white w-full"
+            />
+            <div className="border-t-2 w-full mt-2 border-grey-medium opacity-50"></div>
+            <div className="text-grey-light mt-2 text-xs">
+              min. 2 - max. 100.000
+            </div>
+            <div className={styles.error}>
+              <ErrorMessage name="licenses" />
+            </div>
+          </div>
+          <div className="text-white font-bold text-sm mt-10 mb-2">
+            Secondary sales distribution
+          </div>
+          <div className="text-grey-light leading-6">
+            Everytime the license generate royalty, the royalty will be
+            distributed between Master creator and owner. You are Master owner
+            by default.
+          </div>
+          <div className="mt-6 w-full">
+            <Field
+              id="creatorOwnerSplit"
+              name="creatorOwnerSplit"
+              placeholder="30"
+              className="outline-none bg-grey-dark text-white w-full"
+            />
+            <div className="border-t-2 w-full mt-2 border-grey-medium opacity-50"></div>
+            <div className="text-grey-light mt-2 text-xs">
+              Example: if you type in 30, 30% will go to the creator and 70%
+              will go to the owner of this NFT. Max value 100.
+              <div className={styles.error}>
+                <ErrorMessage name="creatorOwnerSplit" />
+              </div>
+            </div>
+          </div>
+          <button className={styles.mintButton} type="submit">
+            Mint NFT
+          </button>
+        </div>
+      </Form>
+    </Formik>
+  )
+
+  const renderInformationFirstStep = () => (
+    <div className={styles.informationStepWrapper}>
+      <div className={styles.informationStepContent}>
+        <div className={styles.informationStepHeader}>Upload music</div>
+        <div className={styles.informationStepBody}>
+          You are obligated to publish only musics that you have full right on
+          them. As soon as you mint your music as an NFT, you music is on
+          blockchain and no one can remove it anymore.
+        </div>
+      </div>
+    </div>
+  )
+
+  const renderInformationSecondStep = () => (
+    <div className={styles.informationStepWrapper}>
+      <div className={styles.informationStepContent}>
+        <div className={styles.informationStepHeader}>Master NFT</div>
+        <div className={styles.informationStepBody}>
+          Your Master is a unique 1/1 valuable NFT. Owner of your Master will
+          own initially all the unsold Licenses and he get royalty on every
+          trade on each Licenses
+        </div>
+      </div>
+      <div className={cn(styles.informationStepContent, 'mt-10')}>
+        <div className={styles.informationStepHeader}>License NFT</div>
+        <div className={styles.informationStepBody}>
+          All your License NFTs belong to the owner of the Master NFT initially.
+          Your fans can use, buy, trade, collect or monitise your Licenses and
+          everytime they earn money with them, creator and Master owner will
+          earn also money with them automatically.
+        </div>
+      </div>
+    </div>
+  )
+
   return (
     <>
-      <div className={styles.wrapperStyle}>
-        <Formik
-          initialValues={initialValues}
-          validationSchema={validationSchema}
-          onSubmit={onSubmit}
-          enableReinitialize
-        >
-          <Form>
-            <div className="flex flex-col">
-              <div className="text-white font-bold text-base">Track</div>
-              <div>
-                <label htmlFor="nftFile" className={styles.buttonStyle}>
-                  Choose Music File
-                </label>
-                <Field
-                  type="file"
-                  id="nftFile"
-                  name="nftFile"
-                  className="hidden"
-                  onChange={(e) =>
-                    onFileChange(
-                      e,
-                      setNftFile,
-                      setNftFileError,
-                      SUPPORTED_FORMATS_MUSIC
-                    )
-                  }
-                ></Field>
-                <div className="text-grey-light mt-3 text-xs">
-                  MP3, WAVE - Max 100Mb
-                </div>
-                <div className="text-grey-light text-xs">
-                  {nftFile && `Selected File: ${nftFile.name}`}
-                </div>
-                <div className={styles.error}>{nftFileError}</div>
-              </div>
-              <div>
-                <label htmlFor="pictureFile" className={styles.buttonStyle}>
-                  Choose Cover Picture
-                </label>
-                <input
-                  type="file"
-                  id="pictureFile"
-                  name="pictureFile"
-                  className="hidden"
-                  onChange={(e) =>
-                    onFileChange(
-                      e,
-                      setPictureFile,
-                      setPictureFileError,
-                      SUPPORTED_FORMATS_PICTURE
-                    )
-                  }
-                ></input>
-                <div className="text-grey-light mt-3 text-xs">
-                  JPG, PNG - Max 100Mb
-                </div>
-              </div>
-              <div className="text-grey-light text-xs">
-                {pictureFile && `Selected File: ${pictureFile.name}`}
-              </div>
-              <div className={styles.error}>{pictureFileError}</div>
-              <div className="text-white font-bold text-sm mt-10">
-                Track Name
-              </div>
-              <div className="mt-3 w-full">
-                <Field
-                  id="name"
-                  name="name"
-                  placeholder="Ice in the dark..."
-                  className="outline-none bg-grey-dark text-white w-full"
-                />
-                <div className="border-t-2 w-full mt-2 border-grey-medium opacity-50"></div>
-                <div className="text-grey-light mt-2 text-xs">
-                  max. 20 characters
-                </div>
-                <div className={styles.error}>
-                  <ErrorMessage name="name" />
-                </div>
-              </div>
-
-              <div className="text-white font-bold text-sm mt-10">Licenses</div>
-              <div className="mt-3 w-full">
-                <Field
-                  id="licenses"
-                  name="licenses"
-                  placeholder="2"
-                  className="outline-none bg-grey-dark text-white w-full"
-                />
-                <div className="border-t-2 w-full mt-2 border-grey-medium opacity-50"></div>
-                <div className="text-grey-light mt-2 text-xs">
-                  min. 2 - max. 100.000
-                </div>
-                <div className={styles.error}>
-                  <ErrorMessage name="licenses" />
-                </div>
-              </div>
-              <div className="text-white font-bold text-sm mt-10">
-                Master Royalty Fees
-              </div>
-              <div className="mt-3 w-full">
-                <Field
-                  id="royaltyFeeMaster"
-                  name="royaltyFeeMaster"
-                  placeholder="2"
-                  className="outline-none bg-grey-dark text-white w-full"
-                />
-                <div className="border-t-2 w-full mt-2 border-grey-medium opacity-50"></div>
-                <div className="text-grey-light mt-2 text-xs">
-                  Enter a number between 0 and 100
-                  <div className={styles.error}>
-                    <ErrorMessage name="royaltyFeeMaster" />
-                  </div>
-                </div>
-              </div>
-              <div className="text-white font-bold text-sm mt-10">
-                License Royalty Fees
-              </div>
-              <div className="mt-3 w-full">
-                <Field
-                  id="royaltyFeeLicense"
-                  name="royaltyFeeLicense"
-                  placeholder="2"
-                  className="outline-none bg-grey-dark text-white w-full"
-                />
-                <div className="border-t-2 w-full mt-2 border-grey-medium opacity-50"></div>
-                <div className="text-grey-light mt-2 text-xs">
-                  Enter a number between 0 and 100
-                  <div className={styles.error}>
-                    <ErrorMessage name="royaltyFeeLicense" />
-                  </div>
-                </div>
-              </div>
-              <div className="text-white font-bold text-sm mt-10">
-                Creator Owner - Split
-              </div>
-              <div className="mt-3 w-full">
-                <Field
-                  id="creatorOwnerSplit"
-                  name="creatorOwnerSplit"
-                  placeholder="2"
-                  className="outline-none bg-grey-dark text-white w-full"
-                />
-                <div className="border-t-2 w-full mt-2 border-grey-medium opacity-50"></div>
-                <div className="text-grey-light mt-2 text-xs">
-                  Enter a number between 0 and 100
-                  <div className={styles.error}>
-                    <ErrorMessage name="creatorOwnerSplit" />
-                  </div>
-                </div>
-              </div>
-              <div className="text-white font-bold text-sm mt-10">
-                Description
-              </div>
-              <div className="mt-3">
-                <Field
-                  type="input"
-                  as="textarea"
-                  name="description"
-                  className={styles.descriptionField}
-                  id="trac-desc"
-                  placeholder="I am ..."
-                  rows={8}
-                  cols={50}
-                ></Field>
-                <div className={styles.error}>
-                  <ErrorMessage name="description" />
-                </div>
-              </div>
-              <button className={styles.mintButton} type="submit">
-                Mint NFT
-              </button>
-            </div>
-          </Form>
-        </Formik>
+      <div className={styles.stepWrapper}>
+        <div className={styles.wrapperStyle}>
+          {!showSecondStep ? renderFirstStep() : renderSecondStep()}
+        </div>
+        {!showSecondStep
+          ? renderInformationFirstStep()
+          : renderInformationSecondStep()}
       </div>
       <Modal
         isOpen={showing}
