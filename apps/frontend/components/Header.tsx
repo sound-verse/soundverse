579cdaf0779c815bff2aa6c1ce4a3d9cc12b20bd
MichaelLod <michael.lodzik@gmail.com>
2022-07-30 09:34:38 +0200

    fix: styles

--- apps/frontend/components/Header.tsx
+++ apps/frontend/components/Header.tsx
@@ -94,55 +94,72 @@ const Header = ({ className = '' }) => {
         <div className={styles.logo}>
           <Link href="/soundverses" passHref>
             <a>
-              <img src="/logo-black.svg" alt="next" />
+              <div className="relative w-40 h-12">
+                <Image src="/logo-black.svg" alt="next" layout="fill" />
+              </div>
             </a>
           </Link>
         </div>
-        <div className="flex justify-start">
-        <div className={styles.linkWrapper}>
+        <div className="flex justify-center items-center">
+          <div className={styles.linkWrapper}>
+            <div className="flex items-center">
+              <div className=" w-[20px] h-[20px] relative">
+                <Image
+                  className=""
+                  src="/img/home.svg"
+                  alt="next"
+                  layout="fill"
+                />
+              </div>
+              <Link href="/soundverses" passHref>
+                <p className={styles.marketplaceLink}>Home</p>
+              </Link>
+            </div>
 
-          <div class="flex items-center">
-            <img class="w-[20px]  h-[20px]" src="/icons8-home.svg" alt="next" />
-          <Link href="/soundverses" passHref>
-            <p className={styles.marketplaceLink}>Home</p>
-          </Link>
-          </div>
+            <div className="flex items-center">
+              <div className=" w-[30px]  h-[30px] relative">
+                <Image
+                  className=""
+                  src="/img/dollar.svg"
+                  alt="next"
+                  layout="fill"
+                />
+              </div>
+              <Link href="/marketplace" passHref>
+                <p className={styles.marketplaceLink}>Market</p>
+              </Link>
+            </div>
 
+            <div className="flex items-center">
+              <div className=" w-[20px]  h-[20px] relative">
+                <Image
+                  className=""
+                  src="/img/sparkling.svg"
+                  alt="next"
+                  layout="fill"
+                />
+              </div>
+              <Link href="/mint" passHref>
+                <p className={styles.marketplaceLink}>Mint</p>
+              </Link>
+            </div>
 
-          <div class="flex items-center ">
-            <img className="w-[30px]  h-[30px]" src="/icons8-dollar-coin.svg" alt="next"/>
-            <Link href="/marketplace" passHref>
-              <p className={styles.marketplaceLink}>Market</p>
-            </Link>
-          </div>
-
-
-
-
-
-<div class="flex items-center ">
-  <img className="w-[20px]  h-[20px]" src="/icons8-sparkling.svg" alt="next"/>
-  <Link href="/mint" passHref>
-    <p className={styles.marketplaceLink}>Mint</p>
-  </Link>
-</div>
-
-
-
-          <div class="flex items-center">
-
-            <img className="w-[25px]  h-[25px]" src="/icons8-launch.svg" alt="next"/>
-            <Link href="/launch" passHref>
-              <p className={styles.marketplaceLink}>Launch</p>
-            </Link>
-
-
+            <div className="flex items-center">
+              <div className="w-[25px]  h-[25px] relative">
+                <Image
+                  className=""
+                  src="/img/launch.svg"
+                  alt="next"
+                  layout="fill"
+                />
+              </div>
+              <Link href="/launch" passHref>
+                <p className={styles.marketplaceLink}>Launch</p>
+              </Link>
+            </div>
           </div>
         </div>
-
-
-
-
+        <div>
           <div className={styles.connectButtonWrapper}>
             <button
               className={styles.connectButton}
