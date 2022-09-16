37963f778d9d1451dfa3ff01ed40ce1da441efd3
MichaelLod <michael.lodzik@gmail.com>
2022-09-16 09:19:49 +0200

    switch to matic logo

--- apps/frontend/components/marketplace/SoundCard.tsx
+++ apps/frontend/components/marketplace/SoundCard.tsx
@@ -168,7 +168,7 @@ function SoundCard({
                         nft.sellings.masterSelling.mintVoucher.currency} */}
                       <div className="relative w-3 h-4">
                         <Image
-                          src="/img/ethIconWhite.svg"
+                          src="/img/maticLogo.svg"
                           layout="fill"
                           alt="Ethereum Logo"
                         />
@@ -200,7 +200,7 @@ function SoundCard({
                   </div>
                   <div className="relative w-3 h-4">
                     <Image
-                      src="/img/ethIconWhite.svg"
+                      src="/img/maticLogo.svg"
                       layout="fill"
                       alt="Ethereum Logo"
                     />
