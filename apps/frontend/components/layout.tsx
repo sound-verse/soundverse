208baf576efe6920ce1233ff00b69379f3636e98
Michael Lodzik <michael.lodzik@gmail.com>
2022-09-24 01:50:28 +0200

    Update layout.tsx

--- apps/frontend/components/layout.tsx
+++ apps/frontend/components/layout.tsx
@@ -17,19 +17,16 @@ export default function Layout({ className = '', children }) {
   }, [isMobile])
 
   return (
-     <div className="flex items-center justify-center w-full h-screen">
-       We will be on mainnet in a few days, sit tight!
-     </div>
-   // <div>
-   //   {showMobileMenu ? <MobileHeader /> : <Header />}
-   //   <div
-   //     className={cn(
-   //       'mx-auto sm:max-w-screen-sm md:max-w-screen-md lg:max-w-screen-lg xl:max-w-screen-xl 2xl:max-w-screen-2xl',
-   //       className
-   //     )}
-   //   >
-   //     <div className="p-4 sm:p-10">{children}</div>
-    //  </div>
-   // </div>
+    <div>
+      {showMobileMenu ? <MobileHeader /> : <Header />}
+      <div
+        className={cn(
+          'mx-auto sm:max-w-screen-sm md:max-w-screen-md lg:max-w-screen-lg xl:max-w-screen-xl 2xl:max-w-screen-2xl',
+          className
+        )}
+      >
+        <div className="p-4 sm:p-10">{children}</div>
+      </div>
+    </div>
   )
 }
