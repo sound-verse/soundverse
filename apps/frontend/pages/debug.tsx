771c465c376883192aff0a99ecef231a6201d286
MichaelLod <michael.lodzik@gmail.com>
2022-10-17 10:03:55 +0200

    debug: audio context

--- apps/frontend/pages/debug.tsx
+++ apps/frontend/pages/debug.tsx
@@ -2,34 +2,68 @@ import React, { useEffect, useRef } from 'react'
 import Head from 'next/head'
 import Layout from '../components/layout'
 import { useRouter } from 'next/router'
-import { useAudioContext } from '../context/AudioContext'
 
 export default function Debug() {
-  const { setAudio, currentTrack, setCurrentTrack, play } = useAudioContext()
-  const wavesurferRef = useRef(null)
+  const waveformRef = useRef(null)
+  const wavesurfer = useRef(null)
+  const WavesurferLibrary = useRef(null)
+
+  const formWaveSurferOptions = (ref) => ({
+    container: ref,
+    waveColor: 'grey',
+    progressColor: 'black',
+    cursorColor: 'black',
+    height: 50,
+    pixelRatio: 1,
+    normalize: true,
+    barWidth: 1,
+    barGap: 1,
+    backend: 'MediaElement',
+  })
 
   useEffect(() => {
-    setCurrentTrack({ wavesurferRef: wavesurferRef.current })
-  }, [setCurrentTrack])
+    if (!wavesurfer.current) {
+      //   create()
+    }
+  }, [])
+
+  const create = async () => {
+    if (!WavesurferLibrary.current) {
+      WavesurferLibrary.current = await (await import('wavesurfer.js')).default
+    }
+
+    if (wavesurfer.current) {
+      await wavesurfer.current.destroy()
+    }
+
+    wavesurfer.current = await new WavesurferLibrary.current.create(
+      formWaveSurferOptions(waveformRef.current)
+    )
+
+    wavesurfer.current.load('/dummy/dummy.mp3')
+  }
 
   return (
     <div>
       <Layout>
-        <div ref={wavesurferRef} />
+        <div ref={waveformRef} />
 
         <div className=" text-white w-full flex items-center justify-center flex-col">
           <div
             className="p-5 bg-black rounded-full flex items-center justify-center mt-20"
             onClick={async () => {
-              await setAudio('/dummy/dummy.mp3')
-              setTimeout(() => {
-                currentTrack.wavesurfer.play()
-              }, 100)
+              await create()
+              wavesurfer.current.play()
             }}
           >
             PLAY
           </div>
-          <div className="p-5 bg-black rounded-full flex items-center justify-center mt-20">
+          <div
+            className="p-5 bg-black rounded-full flex items-center justify-center mt-20"
+            onClick={() => {
+              wavesurfer.current.pause()
+            }}
+          >
             Pause
           </div>
         </div>
