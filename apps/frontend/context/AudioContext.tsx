8a6c4aa890c263f467aab87a0b058ea847e9c38b
MichaelLod <michael.lodzik@gmail.com>
2022-10-17 17:44:37 +0200

    fix: audio player bar

--- apps/frontend/context/AudioContext.tsx
+++ apps/frontend/context/AudioContext.tsx
@@ -5,8 +5,11 @@ import React, {
   useReducer,
   useContext,
   createContext,
+  useRef,
+  useEffect,
 } from 'react'
 import { NftType } from '../common/graphql/schema.d'
+import useWindowDimensions from '../hooks/useWindowDimensions'
 
 export enum PLAYER_STATUS {
   PLAYING = 'playing',
@@ -17,8 +20,6 @@ export type Track = {
   id?: string
   contractAddress?: string
   trackName?: string
-  url?: string
-  waveForm?: number[]
   currentPosition?: number
   creatorName?: string
   trackPictureUrl?: string
@@ -26,14 +27,30 @@ export type Track = {
   visible?: boolean
   mute?: boolean
   volume?: number
-  play?: boolean
   isLoading?: boolean
   playTime?: number
   isPlaying?: boolean
   nftType?: NftType
   restart?: boolean
   isRoomPlayer?: boolean
+  waveDiv?: any
+  wavesurferRef?: any
+  playOnLoad?: boolean
 }
+
+const formWaveSurferOptions = (ref) => ({
+  container: ref,
+  waveColor: 'grey',
+  progressColor: 'black',
+  cursorColor: 'black',
+  height: 50,
+  pixelRatio: 1,
+  normalize: true,
+  barWidth: 1,
+  barGap: 1,
+  backend: 'MediaElement',
+  responsive: true,
+})
 
 export type State = {
   currentTrack: Track
@@ -41,22 +58,26 @@ export type State = {
 
 type AudioContextType = State & {
   setCurrentTrack: (track: Track) => void
+  setAudio: (
+    audioUrl: string,
+    waveForm: number[],
+    isRoomPlayer?: boolean
+  ) => void
+  play: () => void
+  pause: () => void
+  gotoTrackPosition: (trackPosition: number, playTime: number) => void
 }
 
-type Action =
-  | {
-      type: 'SET_CURRENT_TRACK'
-      track: Track
-    }
-  | { type: 'SET_VOLUME'; volume: number }
+type Action = {
+  type: 'SET_CURRENT_TRACK'
+  track: Track
+}
 
 const initialState: State = {
   currentTrack: {
     id: '',
     contractAddress: '',
     trackName: '',
-    url: '',
-    waveForm: [0],
     currentPosition: 0,
     creatorEthAddress: '',
     creatorName: '',
@@ -70,6 +91,7 @@ const initialState: State = {
     restart: false,
     nftType: NftType.Master,
     isRoomPlayer: false,
+    playOnLoad: false,
   } as Track,
 }
 
@@ -93,23 +115,123 @@ const audioReducer = (state: State, action: Action): State => {
 
 export const AudioProvider: FC = (props) => {
   const [state, dispatch] = useReducer(audioReducer, initialState)
+  const { isMobile } = useWindowDimensions()
+  const wavesurferRef = useRef(null)
+  const wavesurferLibrary = useRef(null)
+  const wavesurfer = useRef(null)
+
+  const currentTrack = useMemo(() => state.currentTrack, [state.currentTrack])
+
+  useEffect(() => {
+    if (isMobile) {
+      setAudio('/dummy/dummy.mp3', [])
+    }
+  }, [])
+
+  useEffect(() => {
+    if (!wavesurfer.current) {
+      return
+    }
+    wavesurfer.current.setMute(currentTrack.mute)
+  }, [currentTrack.mute])
+
+  useEffect(() => {
+    if (!wavesurfer.current) {
+      return
+    }
+    wavesurfer.current.setVolume(currentTrack.volume)
+  }, [currentTrack.volume])
 
   const setCurrentTrack = useCallback(
     (track: Track) => dispatch({ type: 'SET_CURRENT_TRACK', track }),
     [dispatch]
   )
 
-  const currentTrack = useMemo(() => state.currentTrack, [state.currentTrack])
+  const gotoTrackPosition = useCallback(
+    (trackPosition: number, playTime: number) => {
+      let seekToValue = trackPosition / playTime
+
+      if (seekToValue > 1 || seekToValue < 0 || isNaN(seekToValue)) {
+        seekToValue = 0
+      }
+
+      wavesurfer.current.seekTo(seekToValue)
+    },
+    [currentTrack.playTime]
+  )
+
+  const setAudio = useCallback(
+    async (audioUrl: string, waveForm: number[], isRoomPlayer = false) => {
+      if (!wavesurferLibrary.current) {
+        wavesurferLibrary.current = await (
+          await import('wavesurfer.js')
+        ).default
+      }
+
+      if (wavesurfer.current) {
+        await wavesurfer.current.destroy()
+        setCurrentTrack({ isPlaying: false })
+      }
+
+      const options = formWaveSurferOptions(
+        currentTrack?.wavesurferRef ?? wavesurferRef.current
+      )
+
+      wavesurfer.current = await new wavesurferLibrary.current.create({
+        ...options,
+        ...(isRoomPlayer && { interact: false }),
+      })
+
+      await wavesurfer.current.load(audioUrl, waveForm)
+
+      await new Promise((resolve) => {
+        wavesurfer.current.on('ready', () => {
+          setCurrentTrack({ isLoading: false })
+          resolve(true)
+        })
+      })
+    },
+    [state]
+  )
+
+  const play = useCallback(() => {
+    if (!wavesurfer.current) {
+      return
+    }
+
+    wavesurfer.current.setMute(currentTrack.mute)
+    wavesurfer.current.setVolume(currentTrack.volume)
+
+    wavesurfer.current.play()
+    setCurrentTrack({ isPlaying: true, visible: true })
+  }, [state])
+
+  const pause = useCallback(() => {
+    if (!wavesurfer.current) {
+      return
+    }
+    wavesurfer.current.pause()
+    setCurrentTrack({ isPlaying: false })
+  }, [state])
 
   const value = useMemo(
     () => ({
       currentTrack,
       setCurrentTrack,
+      play,
+      pause,
+      setAudio,
+      gotoTrackPosition,
     }),
-    [currentTrack, setCurrentTrack]
+    [currentTrack, pause, play, setCurrentTrack, setAudio, gotoTrackPosition]
   )
 
-  return <AudioContext.Provider value={value} {...props} />
+  return (
+    <AudioContext.Provider value={value}>
+      <div {...props} />
+      <div ref={wavesurferRef} className="hidden" />
+    </AudioContext.Provider>
+  )
 }
 
 export const useAudioContext = () => {
